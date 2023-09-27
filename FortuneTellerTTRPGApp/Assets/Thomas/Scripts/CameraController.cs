74c4d64f911a2b2f8c6be37ea026e3388447d262
Zatomate <duthoit.thomas9@gmail.com>
2023-09-27 18:25:52 +0200

    Upgraded Camera Controls

--- FortuneTellerTTRPGApp/Assets/Thomas/Scripts/CameraController.cs
+++ FortuneTellerTTRPGApp/Assets/Thomas/Scripts/CameraController.cs
@@ -6,23 +6,31 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float cameraSizeTarget = 3.0f;    
     [SerializeField] private bool drag = false;
     [SerializeField] private bool resettingCameraPos = false;
+    [SerializeField] private float scrollSpeed = 10.0f;
+    [SerializeField] private float dragSpeed = 15.0f;
+    [SerializeField] private float cameraResetSpeed = 3.0f;
+
+    private float actualCameraMovingSpeed = 0;
     
     private Vector3 cameraDefaultPos;
-    [SerializeField] private Vector3 cameraOrigin;
-    [SerializeField] private Vector3 cameraPosDelta = Vector3.zero;
+    private Vector3 cameraOrigin;
+    private Vector3 cameraPosDelta = Vector3.zero;
+    private Vector3 cameraPosTarget;
 
     private void Awake() {
         cameraDefaultPos = transform.position;
+        cameraPosTarget = cameraDefaultPos;
     }
 
     private void Update() {
-        Vector2 scrollDelta = Input.mouseScrollDelta;
-        cameraSizeTarget -= scrollDelta.y/3;
+        cameraSizeTarget -= Input.mouseScrollDelta.y/3;
         cameraSizeTarget = Mathf.Clamp(cameraSizeTarget, 1, 500);
-        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, cameraSizeTarget, 10.0f * Time.deltaTime);
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, cameraSizeTarget, scrollSpeed * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.R)) {
+        if (Input.GetKey(KeyCode.Space)) {
             resettingCameraPos = true;
+            cameraPosTarget = cameraDefaultPos;
+            actualCameraMovingSpeed = cameraResetSpeed;
         }
 
         if (Input.GetMouseButton(0)) {
@@ -31,17 +39,18 @@ public class CameraController : MonoBehaviour
             }
             if (!drag) { 
                 drag = true; 
+                actualCameraMovingSpeed = dragSpeed;
                 cameraOrigin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 }
             cameraPosDelta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - cameraTransform.position;
         } else { drag = false; }
 
         if (drag) {
-            cameraTransform.position = cameraOrigin - cameraPosDelta;
+            cameraPosTarget = cameraOrigin - cameraPosDelta;
         }
+        cameraTransform.position = Vector3.Lerp(cameraTransform.position, cameraPosTarget, actualCameraMovingSpeed * Time.deltaTime);
 
         if (resettingCameraPos) {
-            cameraTransform.position = Vector3.Lerp(cameraTransform.position, cameraDefaultPos, 3.0f * Time.deltaTime);
             if (Vector3.Distance(cameraTransform.position, cameraDefaultPos) < 0.001) { resettingCameraPos = false; }
         }
     }
